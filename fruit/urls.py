2583994e4a0a24e956bcacea5f10a2b0603e4fee
Lizanoskova <enoskovamipt@gmail.com>
2018-08-12 18:13:20 +0300

    add templates

---
+++ fruit/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from fruit.views import *
+
+urlpatterns = [
+    url(r'^$', Fruit_list.as_view(), name="fruitview"),
+    url(r'^(?P<pk>\d+)/$', fruit_page, name="currentfruit"),
+]
